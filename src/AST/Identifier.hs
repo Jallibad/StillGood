851023cf409c7ae49b323b57c1f66f748ad8543c
Jordan <jallibad@gmail.com>
2019-02-22 15:48:02 -0500

    Added IsString instance to Identifier

--- src/AST/Identifier.hs
+++ src/AST/Identifier.hs
@@ -1,10 +1,13 @@
 module AST.Identifier where
 
 import Data.Aeson
+import Data.String (IsString, fromString)
 import GHC.Generics (Generic)
 
 newtype Identifier = Identifier String deriving (Generic, Eq, Ord)
 deriving instance Show Identifier
+instance IsString Identifier where
+	fromString = Identifier
 instance ToJSON Identifier where
 	toEncoding = genericToEncoding defaultOptions
 instance FromJSON Identifier
