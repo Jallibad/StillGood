9e75e8ab5ecd3e83ed4edc51f5d464a415330bab
Shoshana Malfatto <shanalily@gmail.com>
2019-03-01 01:42:19 -0500

    Update Precedence.hs

--- src/AST/Precedence.hs
+++ src/AST/Precedence.hs
@@ -49,3 +49,25 @@ sya stack queue ((expression, prec) : tokenList) = case prec of
 	Infix n _a	-> sya ((expression, n) : remainder) (popToQueue parentOperators queue) tokenList
 		where (parentOperators, remainder) = break ((>n) . snd) stack
 sya stack queue [] = popToQueue stack queue
+
+
+-- pretty sure this is all wrong, I need to work on this [TODO]
+rpn_apply :: Expression -> [Expression] -> Expression
+rpn_apply op exprs =
+	case (Data.Foldable.length exprs) of
+		0 -> undefined -- error
+		1 -> (Application op (head exprs)) -- one arg left
+		_ -> (Application (rpn_apply op (tail exprs)) (head exprs))
+
+-- this needs to apply to various kinds of types, where an operator can be treated as an operand
+rpn2 :: [Expression] -> [Expression] -> Expression
+rpn2 stack (rpnExpr : rpnExprList) = case rpnExpr of
+	-- ExplicitType (HindleyMilner.Type.Variable x) v@(BuiltIn _) -> -- should be BuiltIn Variable type 
+	-- 	rpn2 (rpnExpr : stack) rpnExprList -- add to stack
+	ExplicitType (Arrow a b) op@(BuiltIn _)	-> rpn2 ((rpn_apply op args) : remainder) rpnExprList
+		where (args, remainder) = (Prelude.take (numArgs (Arrow a b)) stack, Prelude.drop (numArgs (Arrow a b)) stack)  -- BuiltIn operator
+	BuiltIn _ -> rpn2 (rpnExpr : stack) rpnExprList
+	_ -> undefined -- error
+rpn2 stack [] = case (Data.Foldable.length stack) of
+	0 -> undefined -- error
+	_ -> head stack -- there should only be one item in stack
