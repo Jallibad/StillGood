a9a313b6a8fc08bf3fd0b0547fed171fd12d07ea
Jordan <jallibad@gmail.com>
2019-03-01 15:15:17 -0500

    Started trying to rewrite Type to allow for recursion-schemes

--- src/AST/Types.hs
+++ src/AST/Types.hs
@@ -14,3 +14,15 @@ data Expression = Variable {identifier :: Identifier}
 instance ToJSON Expression where
 	toEncoding = genericToEncoding defaultOptions
 instance FromJSON Expression
+
+data ExpressionF a = VariableF {identifier :: Identifier}
+				| LambdaF {argument :: Identifier, body :: a}
+				| ApplicationF {function :: a, body :: a}
+				| ExplicitTypeF {annotation :: Type, expression :: a}
+				| BuiltInF String
+				deriving (Generic, Show, Functor)
+
+newtype Term f = In {out :: f (Term f)}
+-- instance ToJSON ExpressionF a where
+-- 	toEncoding = genericToEncoding defaultOptions
+-- instance FromJSON Expression
