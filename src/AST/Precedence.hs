99d1082d672786614dee53ec3471690d0bebc5fd
shanalily <shanalily@gmail.com>
2019-03-01 01:21:45 -0500

    rpn attempt (very much not finished)

--- src/AST/Precedence.hs
+++ src/AST/Precedence.hs
@@ -6,19 +6,21 @@ import AST.Types
 import Control.Monad (MonadPlus)
 import Data.Foldable
 import Data.Sequence
-import HindleyMilner.Type (numArgs)
+import HindleyMilner.Type (Type(Variable,Arrow), numArgs)
 
 type ApplicationRule = forall t. (Foldable t, MonadPlus t) => t Expression -> Expression
 
 data Associativity = L | R deriving (Show)
 data Precedence = Prefix | Infix {level :: Int, assoc :: Associativity} deriving (Show)
 
+-- the rpn rule folds list based on foldingfunction and takes head...
 rpn :: ApplicationRule
 rpn = head . foldl foldingFunction []
 	where
 		foldingFunction xs (ExplicitType (numArgs -> a) f@(BuiltIn _)) = apply (f : Prelude.take a xs) : Prelude.drop a xs
 		foldingFunction _ _ = undefined
 
+-- applies Application to two expressions?
 apply :: ApplicationRule
 apply = foldl1 Application
 
@@ -49,3 +51,33 @@ sya stack queue ((expression, prec) : tokenList) = case prec of
 	Infix n _a	-> sya ((expression, n) : remainder) (popToQueue parentOperators queue) tokenList
 		where (parentOperators, remainder) = break ((>n) . snd) stack
 sya stack queue [] = popToQueue stack queue
+
+-- given 3, 4, + as op=+, args=4,3 -> (Application (Application + 3) 4)
+-- given 5, 2, 4, max as op=max, args = 4,2,5 -> (Application (Application (Application max 5) 2) 4)
+-- how do I know op is infix? how can I use foldl?
+rpn_apply :: Expression -> [Expression] -> Expression
+rpn_apply op exprs =
+	case (Data.Foldable.length exprs) of
+		0 -> undefined -- error
+		1 -> (Application op (head exprs)) -- one arg left
+		_ -> (Application (rpn_apply op (tail exprs)) (head exprs))
+
+rpn2 :: [Expression] -> [Expression] -> Expression
+rpn2 stack (rpnExpr : rpnExprList) = case rpnExpr of
+	-- ExplicitType (HindleyMilner.Type.Variable x) v@(BuiltIn _) -> -- should be BuiltIn Variable type 
+	-- 			rpn2 (rpnExpr : stack) rpnExprList -- add to stack
+	ExplicitType (Arrow a b) op@(BuiltIn _)	->
+				-- rpn2 ((apply (op : args )) : remainder) rpnExprList -- should be ...
+				rpn2 ((rpn_apply op args) : remainder) rpnExprList
+					where (args, remainder) = (Prelude.take (numArgs (Arrow a b)) stack, Prelude.drop (numArgs (Arrow a b)) stack)  -- BuiltIn operator
+	c@(BuiltIn _) -> rpn2 (rpnExpr : stack) rpnExprList
+	_ -> undefined -- error
+rpn2 stack [] = case (Data.Foldable.length stack) of
+	0 -> undefined -- error
+	_ -> head stack -- there should only be one item in stack
+
+-- rpn2 [] [(ExplicitType (Arrow (HindleyMilner.Type.Variable "a") (HindleyMilner.Type.Variable "b")) (BuiltIn "3")), (ExplicitType (Arrow (HindleyMilner.Type.Variable "a") (HindleyMilner.Type.Variable "b")) (BuiltIn "4")), (ExplicitType (Arrow (HindleyMilner.Type.Variable "a") (HindleyMilner.Type.Variable "b")) (BuiltIn "+"))]
+
+
+
+
