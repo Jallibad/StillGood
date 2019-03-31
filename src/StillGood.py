4412b08d443aff6aba2d66a65e0ae8ba9c56612f
Ryan Stillings <rystills@gmail.com>
2019-03-30 21:08:45 -0400

    add naive ast parsing for testing purposes

--- src/StillGood.py
+++ src/StillGood.py
@@ -14,6 +14,28 @@ def getASTFromFile():
      """
     return open(sys.argv[1]).read()
 
+
+def naiveAstToLLVM(jast):
+    """
+    Convert the input json encoded AST to LLVM code, using a naive method for testing purposes
+    JSON jast: the AST in JSON form produced by the main Haskell routine
+    Returns a string containing the LLVM code matching the input json encoded AST
+    """
+    #json indexing
+    function = jast["function"]
+    body = jast["body"] 
+    funcBody = function["body"]
+    
+    #extract function information by keyword
+    funcArg = function["argument"]
+    funcName = funcBody["identifier"]
+    funcContents = body["contents"]
+    print("full jast: {0}\nargument: {1}, name: {2}, contents: {3}".format(jast,funcArg,funcName,funcContents))
+    
+    #build up the llvm code
+    return "{0} {1}({2}) {{\nreturn {3};\n}}".format("int",funcName,funcArg,funcContents)
+    
+
 def exitError(s):
     """
     Display the specified error message and exit the application
@@ -25,11 +47,11 @@ def exitError(s):
 def main():
     if (len(sys.argv) != 2):
         exitError("1 command line argument expected, {0} received. Usage: python StillGood.py inputCodeFile|inputJSONFile")
-    #if we specified a StillGood file, run it through Haskell to get the AST. Otherwise, read the AST directly from the file
+    # if we specified a StillGood file, run it through Haskell to get the AST. Otherwise, read the AST directly from the file
     ast = getASTFromHaskell() if sys.argv[1][-3:] == ".sg" else getASTFromFile()
     jast = json.loads(ast)
-    print(ast,"\n",jast)
-    
+    # now convert the ast to llvm code
+    print(naiveAstToLLVM(jast))
 
 if __name__ == "__main__":
     main()
